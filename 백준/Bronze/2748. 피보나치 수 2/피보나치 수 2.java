076da6f23db15e2b01de01b0f7c929d093d22d3c
주성현 <43128821+juseonghyun@users.noreply.github.com>
2023-12-03 20:47:07 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 212 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long temp1 = 0;
+        long temp2 = 1;
+        long fn = 0;
+
+        if(n == 0){
+            System.out.println(0);
+        } else if (n == 1) {
+            System.out.println(1);
+        } else {
+            for (int i = 2; i <= n; i++) {
+                fn = temp1 + temp2;
+                temp1 = temp2;
+                temp2 = fn;
+            }
+            System.out.println(fn);
+        }
+    }
+}
