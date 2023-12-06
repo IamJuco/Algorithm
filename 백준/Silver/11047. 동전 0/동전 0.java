df3026b92a191ffb3f9635b71e030bf28e0d02f6
주성현 <43128821+juseonghyun@users.noreply.github.com>
2023-12-07 00:25:06 +0900

    [Silver IV] Title: 동전 0, Time: 412 ms, Memory: 400352 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int temp = 0;
+
+        int[] arr = new int[K];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = N - 1; i >= 0 ; i--) {
+            if(K / arr[i] >= 1){
+                temp = temp + (K / arr[i]);
+                K = K % arr[i];
+            }
+        }
+        System.out.println(temp);
+    }
+}
