7cfab076ffca76e3e87ddd10c8e5236811faa9eb
주성현 <43128821+juseonghyun@users.noreply.github.com>
2023-12-11 23:30:49 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 320 ms, Memory: 19448 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int money = sc.nextInt();
+
+            int Quarter = money / 25;
+            money = money % 25;
+
+            int Dime = money / 10;
+            money = money % 10;
+
+            int Nickel = money / 5;
+            money = money % 5;
+
+            int Penny = money;
+
+            System.out.println(Quarter + " " + Dime + " " + Nickel + " " + Penny);
+        }
+    }
+}
