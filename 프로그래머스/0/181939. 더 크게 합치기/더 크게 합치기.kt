1296bd310dcf6a193dd6f0712df34de8c08acda3
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-02-07 15:15:32 +0900

    [level 0] Title: 더 크게 합치기, Time: 10.38 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.kt
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.kt
@@ -1,5 +1,13 @@
-import kotlin.math.*
-
 class Solution {
-    fun solution(a: Int, b: Int): Int = max("$a$b".toInt(), "$b$a".toInt())
+    fun solution(a: Int, b: Int): Int {
+        var answer: Int = 0
+        val sum1 = (a.toString() + b).toInt()
+        val sum2 = (b.toString() + a).toInt()
+       
+        if (sum1 > sum2){
+            answer = sum1
+        } else answer = sum2
+        
+        return answer
+    }
 }
