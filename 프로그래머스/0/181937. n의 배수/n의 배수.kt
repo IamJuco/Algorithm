48a200e0d0febf0fb6f4875c08f72d445be1a4b2
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-02-13 18:46:43 +0900

    [level 0] Title: n의 배수, Time: 0.02 ms, Memory: 60.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(num: Int, n: Int): Int {
+        var answer: Int
+
+        if (num % n == 0){
+            answer = 1
+        } else {
+            answer = 0
+        }
+
+        return answer
+    }
+}
