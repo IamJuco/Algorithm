faf1691895c9e7795c9e802e24062acc553abf5a
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-02-13 15:08:32 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 25.90 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        var answer = intArrayOf()
+
+        answer = arr.filter { it != arr.minOrNull() }.toIntArray()
+
+        if (answer.count() == 0) {
+            answer += -1
+        }
+
+        return answer
+    }
+}
