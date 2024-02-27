cfd1dac6461973d1c7761ba3ac71fc0e414491d7
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-02-27 11:46:15 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.03 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.kt
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.kt
@@ -1,7 +1,6 @@
 class Solution {
     fun solution(myString: String): String {
-        var answer: String = ""
-        answer = myString.lowercase()
+        var answer: String = myString.lowercase()
         return answer
     }
 }
