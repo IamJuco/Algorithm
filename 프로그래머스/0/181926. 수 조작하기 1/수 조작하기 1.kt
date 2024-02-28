9346b142adb52673457926fe88bded07d9f341c9
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-02-28 13:12:26 +0900

    [level 0] Title: 수 조작하기 1, Time: 3.31 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun solution(n: Int, control: String): Int {
+        var answer = n
+
+        for (i in control.indices){
+            if (control[i].equals('w')){
+                answer += 1
+            } else if (control[i].equals('s')){
+                answer -= 1
+            } else if (control[i].equals('d')){
+                answer += 10
+            } else if (control[i].equals('a')){
+                answer -= 10
+            }
+        }
+
+        return answer
+    }
+}
