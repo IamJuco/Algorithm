f55331e2d4ecf31516330166db02249101cc4c84
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-03 18:44:35 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 32.42 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.kt
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.kt
@@ -1,3 +1,7 @@
 class Solution {
-    fun solution(num_list: IntArray): IntArray = num_list.sorted().slice(5 until num_list.size).toIntArray()
+    fun solution(num_list: IntArray): IntArray {
+        var answer = IntArray(num_list.size-5)
+        answer = num_list.sorted().slice(5 ..num_list.size-1).toIntArray()
+        return answer
+    }
 }
