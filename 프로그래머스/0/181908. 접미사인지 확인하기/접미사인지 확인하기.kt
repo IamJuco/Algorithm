a673820b55ac9474998cd4b631a63dbc5cde727d
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-04 11:28:29 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 8.52 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(my_string: String, is_suffix: String): Int {
+        var answer: Int = 0
+
+        if (my_string.endsWith(is_suffix)){
+            answer = 1
+        }
+
+        return answer
+    }
+}
