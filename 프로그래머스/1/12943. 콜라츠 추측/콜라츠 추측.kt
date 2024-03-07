c5b2ba53a0f18dd6d9befa57d00f19f1b5a20e9f
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-07 11:59:24 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
@@ -1,27 +1,23 @@
 class Solution {
-    // 500번 반복할 때까지 1이 되지 않는다면 -1을 반환
     fun solution(num: Int): Int {
         var answer = 0
-        var value = num
-        
-        while(true){
-            if(value == 1){
+        var value = num.toLong()
+
+        while (true){
+            if (value == 1L){
                 break
-            } else if(answer == 500){
-                answer = -1
+            } else if (answer == 500){
+                answer = - 1
                 break
-            } else {
+            } else{
                 answer++
-                if(value % 2 == 0){
-                    value = value / 2
-                } else{
-                    var temp = value.toDouble()
-                    temp = temp * 3 + 1
-                    value = temp.toInt()
+                if (value % 2 == 0L){
+                    value /= 2
+                } else {
+                    value = value * 3 + 1
                 }
             }
         }
-        
         return answer
     }
 }
