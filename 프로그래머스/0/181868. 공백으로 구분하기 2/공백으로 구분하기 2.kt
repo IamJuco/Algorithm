15a253544f97f869bd78f191395944e52c56327b
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-08 09:41:37 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 20.14 ms, Memory: 65 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(my_string: String): Array<String> {
+        var answer: Array<String> = arrayOf<String>()
+
+        my_string.split(" ").forEach {
+            if (it != " " && it != ""){
+                answer += it
+            }
+        }
+        return answer
+    }
+}
