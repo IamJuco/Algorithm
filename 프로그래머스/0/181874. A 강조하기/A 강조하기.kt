f381397b55d1b6d9666f8a53c899b97813321197
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-08 12:01:18 +0900

    [level 0] Title: A 강조하기, Time: 7.51 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.kt
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.kt
@@ -1,10 +1,6 @@
 class Solution {
     fun solution(myString: String): String {
-        var answer: String = ""
-
-        for (i in 0.. myString.length-1){
-                answer += myString[i].lowercase()
-        }
+        var answer: String = myString.lowercase()
         answer = answer.replace("a","A")
         return answer
     }
