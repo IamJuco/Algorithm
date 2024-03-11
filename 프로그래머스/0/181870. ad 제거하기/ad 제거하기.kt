fcc1f954656737ba6a8d675919914dbda266102a
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-11 12:33:14 +0900

    [level 0] Title: ad 제거하기, Time: 17.91 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(strArr: Array<String>): Array<String> {
+        var answer: Array<String> = arrayOf<String>()
+
+        strArr.forEach {
+            if (!it.contains("ad")){
+                answer += it
+            }
+        }
+
+        return answer
+    }
+}
