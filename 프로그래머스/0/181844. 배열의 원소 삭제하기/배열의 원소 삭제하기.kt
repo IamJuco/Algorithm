3caedffcbe70b351a45c1dd472c7d5704414f907
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-11 09:37:55 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 20.48 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.kt
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.kt
@@ -2,8 +2,8 @@ class Solution {
     fun solution(arr: IntArray, delete_list: IntArray): IntArray {
         var answer = arr.toMutableList()
 
-        for (i in delete_list.indices){
-            answer.remove(delete_list[i])
+        delete_list.forEach{
+            answer.remove(it)
         }
 
         return answer.toIntArray()
