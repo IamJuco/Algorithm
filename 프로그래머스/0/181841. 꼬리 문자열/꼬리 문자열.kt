bd1a2f831d5e8781ceed81876daa9af5bf8a5327
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-11 11:36:48 +0900

    [level 0] Title: 꼬리 문자열, Time: 11.33 ms, Memory: 61.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(str_list: Array<String>, ex: String): String {
+        var answer: String = ""
+
+        str_list.forEach { 
+            if (!it.contains(ex)){
+                answer += it
+            }
+        }
+
+        return answer
+    }
+}
