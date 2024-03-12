5447538b24f49021d3de653c0615fe9276b6f875
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-12 09:57:19 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 60.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(n_str: String): String {
+        var answer: String = ""
+        
+        answer = n_str.toInt().toString()
+        
+        return answer
+    }
+}
