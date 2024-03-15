10df2cc4b2c25bd00402da720cbf1dd78c528aac
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-15 15:23:13 +0900

    [level 0] Title: 접미사 배열, Time: 9.15 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.kt
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.kt
@@ -2,10 +2,11 @@ class Solution {
     fun solution(my_string: String): Array<String> {
         var answer: Array<String> = arrayOf<String>()
 
-        answer = my_string.indices.map {
-            my_string.substring(it)
-        }.sorted().toTypedArray()
-
+        for (i in 0..my_string.length-1){
+            answer += arrayOf(my_string.substring(i))
+        }
+        
+        answer.sort()
         return answer
     }
 }
