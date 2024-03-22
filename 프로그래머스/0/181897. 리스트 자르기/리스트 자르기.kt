a5ced6899e3545548776579e3d427484f51c991a
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-22 10:01:40 +0900

    [level 0] Title: 리스트 자르기, Time: 19.96 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.kt
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.kt
@@ -3,24 +3,24 @@ class Solution {
         var answer: IntArray = intArrayOf()
         var temp: IntArray = intArrayOf()
 
-        if (n == 1){
-            answer += num_list.slice(0..slicer[1])
-
-        } else if (n == 2){
-            answer += num_list.slice(slicer[0]..num_list.size-1)
-
-        } else if (n == 3){
-            answer += num_list.slice(slicer[0]..slicer[1])
-
-        } else {
-            answer += num_list.slice(slicer[0]..slicer[1])
-            // answer = 2 3 4 5 6
-            for (i in 0.. answer.size-1 step slicer[2]){
-                temp += answer[i]
+        when(n){
+            1 -> {
+                answer += num_list.slice(0..slicer[1])
             }
-            answer = temp
+            2 -> {
+                answer += num_list.slice(slicer[0]..num_list.size-1)
+            }
+            3 -> {
+                answer += num_list.slice(slicer[0]..slicer[1])
+            }
+            else -> {
+                answer += num_list.slice(slicer[0]..slicer[1])
+                for (i in 0..answer.size-1 step slicer[2]){
+                    temp += answer[i]
+                }
+                answer = temp
+            }
         }
-
 
         return answer
     }
