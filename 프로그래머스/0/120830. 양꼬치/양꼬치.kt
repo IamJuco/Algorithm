2dd759009939f07da18a3dd41d3074fcbafb0788
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-03-27 21:32:29 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(n: Int, k: Int): Int {
+        var answer: Int = 0
+        var count = 0
+
+        for (i in 1..n){
+            if (i % 10 == 0){
+                count++
+            }
+        }
+        answer = (n * 12000) + ((k - count) * 2000)
+
+        return answer
+    }
+}
