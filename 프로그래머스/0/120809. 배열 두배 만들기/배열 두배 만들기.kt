dcc73f7132650a9d24899b8062e22851255d77c5
주성현 <43128821+juseonghyun@users.noreply.github.com>
2024-04-07 23:21:50 +0900

    [level 0] Title: 배열 두배 만들기, Time: 9.85 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(numbers: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+
+        answer += numbers.map { it * 2 }
+
+        return answer
+    }
+}
