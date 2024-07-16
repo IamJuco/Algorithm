ff736d59b65c85b7188ef6f64dcd50a06a9d75d7
JuSeonghyun <43128821+IamJuco@users.noreply.github.com>
2024-07-16 18:34:07 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
@@ -1,12 +1,20 @@
 class Solution {
     fun solution(x: Int): Boolean {
+        var answer = true
         var sum = 0
-        var temp = x
-        
-        while(temp != 0){
-            sum += temp % 10
-            temp /= 10
+        var transStr = x.toString()
+
+        for (i in transStr.indices) {
+            sum += transStr[i].toString().toInt()
         }
-        return(x % sum == 0)
+
+        if (x % sum == 0) {
+            answer = true
+        } else {
+            answer = false
+        }
+
+
+        return answer
     }
 }
