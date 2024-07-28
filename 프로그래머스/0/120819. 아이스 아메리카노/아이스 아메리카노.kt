e9bb44f209485b806f1c63e834351cadfeaf20d6
JuSeonghyun <43128821+IamJuco@users.noreply.github.com>
2024-07-28 21:28:44 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.kt
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.kt
@@ -1,7 +1,9 @@
 class Solution {
     fun solution(money: Int): IntArray {
-        var answer: IntArray = intArrayOf(money / 5500, money % 5500)
-
+        var answer = IntArray(2)
+        val americano = 5500
+        answer[0] = money / americano
+        answer[1] = money % americano
         return answer
     }
 }
