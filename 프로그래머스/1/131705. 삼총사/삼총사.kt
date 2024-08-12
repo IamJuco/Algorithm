7362f54763d58b37a87fb8adff57cdca3dcfa791
JuSeonghyun <43128821+IamJuco@users.noreply.github.com>
2024-08-12 17:16:18 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.kt
+++ 프로그래머스/1/131705. 삼총사/삼총사.kt
@@ -1,11 +1,11 @@
 class Solution {
     fun solution(number: IntArray): Int {
         var answer: Int = 0
-        for (i1 in number.indices){
-            for (i2 in i1 + 1 until number.size){
-                for (i3 in i2 + 1 until number.size){
-                    if (number[i1] + number[i2] + number[i3] == 0){
-                        answer++
+        for (i in 0..number.size - 1) {
+            for (j in i + 1.. number.size - 1) {
+                for (k in j + 1.. number.size - 1) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer ++
                     }
                 }
             }
