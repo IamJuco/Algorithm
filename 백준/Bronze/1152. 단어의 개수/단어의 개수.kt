f0d127b8903ca4d89d876dc62809b07f5f8f157d
JuSeonghyun <43128821+IamJuco@users.noreply.github.com>
2024-10-18 20:57:48 +0900

    [Bronze II] Title: 단어의 개수, Time: 272 ms, Memory: 60432 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.kt
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.kt
@@ -4,6 +4,6 @@ import java.io.InputStreamReader
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val input = br.readLine()
-    val wordCount = input.trim().split(" ").filter { it.isNotBlank() }
+    val wordCount = input.trim().split(" ").filter { it.isNotEmpty() }
     println(wordCount.size)
 }
