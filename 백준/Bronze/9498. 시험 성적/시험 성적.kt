b61ece587d0e5798f23a7f29022473d123beda56
JuSeonghyun <43128821+IamJuco@users.noreply.github.com>
2024-10-18 21:29:42 +0900

    [Bronze V] Title: 시험 성적, Time: 72 ms, Memory: 11888 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val token = StringTokenizer(br.readLine())
+    val input = Integer.parseInt(token.nextToken())
+    val result = when (input) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        in 60..69 -> "D"
+        else -> "F"
+    }
+    println(result)
+}
