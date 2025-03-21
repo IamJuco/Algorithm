4e76c7850a1d3e27d186f6e29af533f1407260d5
JuSeonghyun <43128821+IamJuco@users.noreply.github.com>
2025-03-21 19:15:44 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 23.82 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -1,11 +1,11 @@
 class Solution {
     fun solution(t: String, p: String): Int {
-        var answer: Int = 0
+        var answer = 0
+        val pValue = p.toBigInteger()
 
-        for (i in 0 until t.length - p.length + 1) {
-            val sub = t.substring(i, i + p.length)
-
-            if (sub.toLong() <= p.toLong()) {
+        for (i in 0..t.length - p.length) { // 7 - 3
+            val sub = t.substring(i, i + p.length).toBigInteger()
+            if (sub <= pValue) {
                 answer++
             }
         }
