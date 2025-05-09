b8309ffaa5351fc6ecf960e281020345894644bf
JuSeonghyun <43128821+IamJuco@users.noreply.github.com>
2025-05-09 21:35:39 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 3.97 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
@@ -1,20 +1,12 @@
 class Solution {
     fun solution(left: Int, right: Int): Int {
         var answer: Int = 0
-
-        for (i in left..right){
-            var temp = 0
-
-            for (j in 1..i){
-                if (i % j == 0){
-                    temp++
-                }
+        for (i in left..right) {
+            var count = 0
+            for (j in 1..i) {
+                if (i % j == 0) count++
             }
-            if (temp % 2 == 0){
-                answer += i
-            } else{
-                answer -= i
-            }
+            if (count % 2 == 0) answer += i else answer -= i
         }
         return answer
     }
