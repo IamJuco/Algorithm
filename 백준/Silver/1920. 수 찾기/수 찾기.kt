4678408505f73d15a23d4156443383ab33d4862b
JuSeonghyun <43128821+IamJuco@users.noreply.github.com>
2025-06-26 21:14:03 +0900

    [Silver IV] Title: 수 찾기, Time: 608 ms, Memory: 73680 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.kt
+++ 백준/Silver/1920. 수 찾기/수 찾기.kt
@@ -1,31 +1,29 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-fun binarySearch(arr: List<Int>, target: Int): Boolean {
-    var start = 0
-    var end = arr.size - 1
-
-    while (start <= end) {
-        val mid = (start + end) / 2
-        when {
-            arr[mid] == target -> return true
-            arr[mid] < target -> start = mid + 1
-            else -> end = mid - 1
-        }
-    }
-
-    return false
-}
-
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
+
     val n = br.readLine().toInt()
-    val nNumbers = br.readLine().split(" ").map { it.toInt() }.sorted()
+    val nNumbers = br.readLine().split(" ").map { it.toInt() }
+
+    // HashMap 사용: 숫자를 key로, 존재 여부를 value로 저장
+    val map = mutableMapOf<Int, Boolean>()
+    for (num in nNumbers) {
+        map[num] = true
+    }
+
     val m = br.readLine().toInt()
     val mNumbers = br.readLine().split(" ").map { it.toInt() }
 
-    for (i in 0..mNumbers.size-1) {
-        if (binarySearch(nNumbers, mNumbers[i])) println(1)
-        else println(0)
+    val sb = StringBuilder()
+    for (num in mNumbers) {
+        if (map.containsKey(num)) {
+            sb.append("1\n")
+        } else {
+            sb.append("0\n")
+        }
     }
+
+    print(sb)
 }
