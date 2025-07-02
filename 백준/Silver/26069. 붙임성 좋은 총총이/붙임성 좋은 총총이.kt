4154baf3b4df462e1d9f1fbbdad41f978de43343
JuSeonghyun <43128821+IamJuco@users.noreply.github.com>
2025-07-02 20:04:05 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 116 ms, Memory: 15092 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.kt
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.kt
@@ -4,18 +4,16 @@ import java.io.InputStreamReader
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val t = br.readLine().toInt()
-    val map = mutableMapOf<String,Boolean>()
-    map["ChongChong"] = true
+    val set = mutableSetOf<String>()
+    set.add("ChongChong")
 
     repeat(t) {
         val (a, b) = br.readLine().split(" ")
-        if (map[a] == true || map[b] == true) {
-            map[a] = true
-            map[b] = true
-        } else {
-            map[a] = false
-            map[b] = false
+        if (a in set || b in set) {
+            set.add(a)
+            set.add(b)
         }
     }
-    println(map.filter { it.value }.size)
+
+    println(set.size)
 }
